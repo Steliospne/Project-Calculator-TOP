e99de9e71d91d838fc39d370be97532af8cdcd1b
Stelios <63586679+Steliospne@users.noreply.github.com>
2024-01-16 20:51:57 +0100

    created obj calculate

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+Calc = new Calculator;
 numpad = document.querySelector('.calc_numpad');
 
-numpad.addEventListener('click',(e)=>console.log(e.srcElement.textContent))
+numpad.addEventListener('click',Calc.calculate)
+
+
+
+
+function Calculator() {
+    
+    this.methods = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b
+    };
+
+    this.calculate = function(e) {
+        console.log(e.srcElement.textContent)
+
+    }
+}
