eb8af3b10b0007efbfe4e68cdbed08571e86a4dc
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-01-17 17:09:41 +0100

    completely different approach

--- script.js
+++ script.js
@@ -1,74 +1,46 @@
-Calc = new Calculator();
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let mustResetScreen = false
 
-const numpad = document.querySelector(".calc_numpad");
-const display_last = document.querySelector(".calc_op_last");
-const display_current = document.querySelector(".calc_op_current");
+const numpad = document.querySelector(".calc_numpad")
+const display_last = document.querySelector(".calc_op_last")
+const display_current = document.querySelector(".calc_op_current")
+const numButtons = document.querySelectorAll(".num-btn")
+const opButtons = document.querySelectorAll(".operator-btn")
+const clearButton = document.querySelector(".clear-btn")
+const delButton = document.querySelector(".del-btn")
 
-numpad.addEventListener("click", (e) => Calc.calculate(e));
+// numpad.addEventListener("click", (e) => Calc.calculate(e))
+clearButton.addEventListener('click', clearScreen)
+delButton.addEventListener('click', delNumber)
 
-function Calculator() {
-    const op = ["+", "-", "*", "÷"];
-    let a = "0";
-    let b = "";
-    let result = 0;
-    let stored_op = "";
+numButtons.forEach((button) => button.addEventListener('click',() => addNum(button.textContent)))
+opButtons.forEach((button) => button.addEventListener('click', () => operate(button.textContent)))
 
-    this.methods = {
-        "+": (a, b) => a + b,
-        "-": (a, b) => b - a,
-        "*": (a, b) => a * b,
-        "÷": (a, b) => b / a,
-    };
+function addNum(number) {
+    if (display_current.textContent === "0" || mustResetScreen) resetScreen()
+    display_current.textContent += number
+}
 
-    this.calculate = function (e) {
-        let btn_input = e.srcElement.textContent;
+function resetScreen () {
+    display_current.textContent = ''
+    mustResetScreen = false
 
-        if (btn_input == "Delete") {
-            a = a.slice(0, -1);
-            display_current.textContent = a;
-        }
+}
 
-        if (btn_input.includes("Clear")) {
-            a = "0";
-            b = "";
-            display_last.textContent = "";
-            display_current.textContent = "0";
-        }
+function clearScreen () {
+    firstOperand = ''
+    secondOperand = ''
+    currentOperation = null
+    display_current.textContent = 0
+    display_last.textContent = ''
+}
 
-        if (
-            !op.includes(btn_input) &&
-            btn_input != "Clear" &&
-            btn_input != "Delete" && btn_input != '='
-        ) {
-            // if (a == "0" && btn_input != ".") a = '';
-            a += btn_input;
-            display_current.textContent = a;
-        }
+function delNumber () {
+    display_current.textContent = display_current.textContent.slice(0, -1)
+}
 
-        if (btn_input == "=" && a != "" && b != "" && stored_op != "=") {
-            display_last.textContent = b + stored_op + a + btn_input;
-            b = this.methods[stored_op](+a, +b);
-            display_current.textContent = b;
-            stored_op = "=";
-            a = b;
-        }
-
-        if (op.includes(btn_input)) {
-            if (b == '' || stored_op == "=") {
-                stored_op = btn_input;
-                b = a;
-                a = "";
-                display_current.textContent = b;
-                display_last.textContent = b + stored_op;
-            }
-
-            if (btn_input != "=" && (b != "") & (a != "")) {
-                b = this.methods[stored_op](+a, +b);
-                a = "";
-                display_last.textContent = b + btn_input;
-                display_current.textContent = b;
-                stored_op = btn_input;
-            }
-        }
-    };
+function operate(operator) {
+    if (currentOperation !== null) evaluate() 
 }
