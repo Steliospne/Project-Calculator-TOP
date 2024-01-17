18db93b9ddd00a3cb33404d0ccf11ef065f359ca
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-01-17 15:17:10 +0100

    still working on the functionality and added classes to all btns

--- script.js
+++ script.js
@@ -7,77 +7,68 @@ const display_current = document.querySelector(".calc_op_current");
 numpad.addEventListener("click", (e) => Calc.calculate(e));
 
 function Calculator() {
-    const op = ["+", "-", "*", "/", "="];
-    let a = "";
+    const op = ["+", "-", "*", "÷"];
+    let a = "0";
     let b = "";
     let result = 0;
-    let stored_op;
+    let stored_op = "";
 
     this.methods = {
         "+": (a, b) => a + b,
-        "-": (a, b) => a - b,
+        "-": (a, b) => b - a,
         "*": (a, b) => a * b,
-        "/": (a, b) => a / b,
+        "÷": (a, b) => b / a,
     };
 
     this.calculate = function (e) {
         let btn_input = e.srcElement.textContent;
 
         if (btn_input == "Delete") {
-            a = a.slice(0,-1);
+            a = a.slice(0, -1);
             display_current.textContent = a;
+        }
+
+        if (btn_input.includes("Clear")) {
+            a = "0";
+            b = "";
+            display_last.textContent = "";
+            display_current.textContent = "0";
         }
 
         if (
             !op.includes(btn_input) &&
             btn_input != "Clear" &&
-            btn_input != "Delete"
+            btn_input != "Delete" && btn_input != '='
         ) {
+            // if (a == "0" && btn_input != ".") a = '';
             a += btn_input;
             display_current.textContent = a;
         }
 
+        if (btn_input == "=" && a != "" && b != "" && stored_op != "=") {
+            display_last.textContent = b + stored_op + a + btn_input;
+            b = this.methods[stored_op](+a, +b);
+            display_current.textContent = b;
+            stored_op = "=";
+            a = b;
+        }
+
         if (op.includes(btn_input)) {
-            if (b == "") {
+            if (b == '' || stored_op == "=") {
+                stored_op = btn_input;
                 b = a;
+                a = "";
+                display_current.textContent = b;
+                display_last.textContent = b + stored_op;
+            }
+
+            if (btn_input != "=" && (b != "") & (a != "")) {
+                b = this.methods[stored_op](+a, +b);
                 a = "";
                 display_last.textContent = b + btn_input;
                 display_current.textContent = b;
                 stored_op = btn_input;
-            } else if (btn_input == "+") {
-                b = this.methods[btn_input](+a, +b);
-                a = "";
-                display_last.textContent = b + btn_input;
-                display_current.textContent = b;
-            } else if (btn_input == "-") {
-                b = this.methods[btn_input](+a, +b);
-                a = "";
-                display_last.textContent = b + btn_input;
-                display_current.textContent = b;
-            } else if (btn_input == "*") {
-                b = this.methods[btn_input](+a, +b);
-                a = "";
-                display_last.textContent = b + btn_input;
-                display_current.textContent = b;
-            } else if (btn_input == "/" || btn_input == "÷") {
-                b = this.methods[btn_input](+a, +b);
-                a = "";
-                display_last.textContent = b + btn_input;
-                display_current.textContent = b;
-            } else if (btn_input == "=") {
-                console.log(stored_op);
-                display_last.textContent = b + stored_op + a + btn_input;
-                b = this.methods[stored_op](+a, +b);
-                a = "";
-                display_current.textContent = b;
             }
         }
-
-        if (btn_input.includes("Clear")) {
-            a = "";
-            b = "";
-            display_last.textContent = "";
-            display_current.textContent = "0";
-        }
     };
-};
+}
